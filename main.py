8458cc278e42a5d893407e7ae47ae1306139fa58
Onevalerione Valerij Dorosh <90389948+Onevalerione@users.noreply.github.com>
2021-12-07 08:48:59 +0300

    Игра - Лабиринт

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
 import sys
 
+
+Purple = (51, 0, 51)
+
 def run():
+
 
     '''инициализируем игру'''
     pygame.init()
@@ -10,7 +14,7 @@ def run():
     screen = pygame.display.set_mode((800, 600))
     ''''задаем название отображаемой области'''
     pygame.display.set_caption('Лабиринт')
-    bg_color = ('Purple')
+    bg_color = (Purple)
 
     '''Создаем бесконечный цикл, в котором будут содержаться все события в игре'''
     while True:
