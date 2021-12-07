512660bd4a9e250e5557c82da8ae18d64ed0924e
Onevalerione Valerij Dorosh <90389948+Onevalerione@users.noreply.github.com>
2021-12-07 08:38:28 +0300

    Игра - Лабиринт

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+import sys
+
+def run():
+
+    '''инициализируем игру'''
+    pygame.init()
+
+    '''создаем отображаемую область для игры'''
+    screen = pygame.display.set_mode((800, 600))
+    ''''задаем название отображаемой области'''
+    pygame.display.set_caption('Лабиринт')
+    bg_color = ('Purple')
+
+    '''Создаем бесконечный цикл, в котором будут содержаться все события в игре'''
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(bg_color)
+        pygame.display.flip()
+
+
+run()
+
+
+
+
+
+
