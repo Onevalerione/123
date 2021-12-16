de4426b3398828da75584d1be2d5dcb7740daf6f
dkova <dakovalyov@miem.hse.ru>
2021-12-16 20:32:39 +0300

    Документация всех действий

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+'''размер игрового экрана, цвета в игре и скорость обновления изображения'''
 WIDTH = 800
 HEIGHT = 600
 BLACK = (0, 0, 0)
@@ -9,16 +10,16 @@ WHITE = (255, 255, 255)
 FPS = 30
 
 class Player(pygame.sprite.Sprite):
-
+    '''функция, отвечающая за начальные данные персонажа игрока'''
     def __init__(self, x, y, img='PL1.png'):
-
         super().__init__()
-
+        '''применяем изображение нпс игрока и устанавливаем персонажа на позицию'''
         self.image = pygame.image.load(img).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-
+        '''устанавливаем начально, что сдвигов не было, мы не трогали стены, 
+        мы не собирали алмазы, спрайты монстров задействованы и персонаж не умер'''
         self.change_x = 0
         self.change_y = 0
         self.walls = None
@@ -31,25 +32,36 @@ class Player(pygame.sprite.Sprite):
         self.monsters = pygame.sprite.Group()
         self.alive = True
 
+    '''функция, отвечающая за движения игрока и его взаимодействие с окружающими объектами'''
     def update(self):
+        '''если был сдвиг по оси x, то мы добавлаем к значению x в координатах
+         положения персонажа игрока значение сдвига по оси х'''
         self.rect.x += self.change_x
+        '''если игрок идет на позицию, где стена, персонаж игрока не будет двигаться'''
         block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
+        '''если выполнено движение по оси x, то в зависомости от значения сдвига, 
+        персонаж игрока пойдет или вправо (x>0), или влево(x<0)'''
         for block in block_hit_list:
 
             if self.change_x > 0:
                 self.rect.right = block.rect.left
             else:
                 self.rect.left = block.rect.right
-
+        '''если был сдвиг по оси y, то мы добавлаем к значению y в координатах
+         положения персонажа игрока значение сдвига по оси y'''
         self.rect.y += self.change_y
+        '''если игрок идет на позицию, где стена, персонаж игрока не будет двигаться'''
         block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
+        '''если выполнено движение по оси y, то в зависомости от значения сдвига, 
+        персонаж игрока пойдет или вверх (y>0), или вниз(y<0)'''
         for block in block_hit_list:
             if self.change_y > 0:
                 self.rect.bottom = block.rect.top
             else:
                 self.rect.top = block.rect.bottom
 
-
+        '''если игрок дошел до алмаза, то он засчитывается в счетчик наколенных алмазов и 
+        уничтожает сам алмаз'''
         diamonds_hit_list = pygame.sprite.spritecollide(self, self.diamonds, False)
         for diamond in diamonds_hit_list:
             self.collected_diamonds += 1
@@ -59,28 +71,27 @@ class Player(pygame.sprite.Sprite):
         for crown in crowns_hit_list:
             self.collected_crowns += 1
             crown.kill()
-
+        '''если игрок дошел до зоны движения монстра, то он умирает'''
         if pygame.sprite.spritecollide(self, self.monsters, False):
             self.alive = False
 
 class Wall(pygame.sprite.Sprite):
+    '''функция, отвечающая за создание стен'''
     def __init__(self, x, y, WIDTH, HEIGHT):
         super().__init__()
-
+        '''формируем стены по установленным ширине и высоте с цветом BLACK'''
         self.image = pygame.Surface([WIDTH, HEIGHT])
         self.image.fill(BLACK)
-
+        '''устанавливаем стены на указанные позиции'''
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
 
-
-
-
 class Diamond(pygame.sprite.Sprite):
+    '''функция, отвечающая за создание алмазов'''
     def __init__(self, x, y, img='Diamond.png'):
         super().__init__()
-
+        '''выгружаем изображение алмаза и устанавливаем несколько на указанные позиции'''
         self.image = pygame.image.load(img).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -96,22 +107,28 @@ class Crown(pygame.sprite.Sprite):
         self.rect.y = y
 
 class Monster(pygame.sprite.Sprite):
+    '''функция, отвечающая за создание монстров'''
     def __init__(self, x, y, img = 'Monster img.png'):
         super().__init__()
+        '''выгружаем изображение монстра и устаналиваем несколько на указанные позиции'''
         self.image = pygame.image.load(img).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-
+        '''устанавливаем зону действия монстра по оси x'''
         self.start = x
         self.stop = x + random.randint(180, 240)
         self.direction = 1
 
+    '''функция, отвечающая за движение монстра'''
     def update(self):
+        '''если начальное положение монстра правее его допустимой зоны действия по оси x,
+        то начальное положение меняется на самую правую зону зону действия'''
         if self.rect.x >= self.stop:
             self.rect.x = self.stop
             self.direction = -1
-
+        '''если начальное положение монстра левее его допустимой зоны действия по оси x,
+        то начальное положение меняется на самую левую зону зону действия'''
         if self.rect.x <= self.start:
             self.rect.x = self.start
             self.direction = 1
@@ -119,17 +136,16 @@ class Monster(pygame.sprite.Sprite):
         self.rect.x += self.direction * 2
 
 
-
-
+'''иниализация запуска игры'''
 pygame.init()
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
 pygame.display.set_caption('Maze')
 
 
-
+'''указываем, что список спрайтов и стен в группе спрайтов '''
 all_sprite_list = pygame.sprite.Group()
 wall_list = pygame.sprite.Group()
-
+'''список стен с координтами x,y и размерами ширина, высота'''
 wall_coords = [
     [0, 0, 800, 10],
     [0, 590, 800, 10],
@@ -139,22 +155,23 @@ wall_coords = [
 
 ]
 
-
+'''добавляем данные о стенах в список стен и список спрайтов'''
 for coord in wall_coords:
     wall = Wall(coord[0], coord[1], coord[2], coord[3])
     wall_list.add(wall)
     all_sprite_list.add(wall)
 
-
+'''указываем, что список алмазов в группе спрайтов и указываем координаты алмазов'''
 diamonds_list = pygame.sprite.Group()
 diamonds_coord = [[100, 140], [235,250], [400,234]]
 
+'''добавляем данные об алмазах в список алмазов и список спрайтов'''
 for coord in diamonds_coord:
     diamond = Diamond(coord[0], coord[1])
     diamonds_list.add(diamond)
     all_sprite_list.add(diamond)
 
-
+'''указываем, что список монстров в группе спрайтов и указываем координаты монстров'''
 
 
 crowns_list = pygame.sprite.Group()
@@ -167,14 +184,17 @@ for coord in crowns_coord:
 
 monsters_list = pygame.sprite.Group()
 monster_coord = [[1, 500], [400, 50]]
+'''добавляем данные об монстрах в список монстров и список спрайтов'''
 for coord in monster_coord:
     monster = Monster(coord[0], coord[1])
     monsters_list.add(monster)
     all_sprite_list.add(monster)
 
-
+'''указываем начальное положение персонажа игрока и добавлаяем и все элементы игры связываем с игроком,
+дабы работало взаимодействие'''
 player = Player(50, 50)
 player.walls = wall_list
+'''добавленяем спрайт персонажа игрока в список спрайтов'''
 all_sprite_list.add(player)
 
 
@@ -183,19 +203,20 @@ player.diamonds = diamonds_list
 player.monsters = monsters_list
 player.crowns = crowns_list
 
+'''Если игра завершена, на экране показываем текст с завершением игры'''
 font = pygame.font.SysFont('Arial', 24, True)
 text = font.render('END', True, WHITE)
 text1 = font.render('Win', True, WHITE)
-
+'''Запуск игры. Добавляем таймер'''
 clock = pygame.time.Clock()
 done = False
 
-
 while not done:
     for event in pygame.event.get():
+        '''если игра завершается, то мы больше ничего не делаем'''
         if event.type == pygame.QUIT:
             done = True
-
+        #если мы запускаем движение, то меняем координаты персонажа игрока
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:
                 player.change_x = 3
@@ -205,7 +226,7 @@ while not done:
                 player.change_y = -3
             elif event.key == pygame.K_s:
                 player.change_y = 3
-
+        #если мы прекращаем движение, то не меняем координаты персонажа игрока
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_d:
                 player.change_x = 0
@@ -216,13 +237,12 @@ while not done:
             elif event.key == pygame.K_s:
                 player.change_y = 0
 
-
+    '''весь фон игры цвета PURPLE'''
     screen.fill(PURPLE)
-
-
-
+    '''если персонаж умер, что выводим текст'''
     if not player.alive:
         screen.blit(text, (100,100))
+    #если это не произошло, то игра продолжается и обновляется
     else:
         all_sprite_list.update()
         all_sprite_list.draw(screen)
@@ -234,11 +254,11 @@ while not done:
 
 
 
-
+    '''Запуск дисплея игры'''
     pygame.display.flip()
     clock.tick(60)
 
-
+'''выходим из игры'''
 pygame.quit()
 
 
