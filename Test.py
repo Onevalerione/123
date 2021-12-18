746d04d00cb2986a0ef7b2ed3b8ac795f70eee30
Onevalerione <onevalerione@mail.ru>
2021-12-18 09:22:27 +0300

    Test Finished

--- Test.py
+++ Test.py
@@ -1,10 +1,10 @@
-
 import pygame
 import unittest
 WIDTH = 800
 HEIGHT = 600
 PURPLE = (51, 0, 51)
 from pygame import display
+
 
 '''Проверяем параметры экрана и их работу'''
 class DisplayModuleTest(unittest.TestCase):
@@ -17,12 +17,20 @@ class DisplayModuleTest(unittest.TestCase):
         display.quit()
 
     def test_update(self):
-
+        #Проверяем, что последовательность типов событий может быть заблокирована
         screen = pygame.display.set_mode([WIDTH,HEIGHT])
         screen.fill(PURPLE)
 
+        r1 = pygame.Rect(0, 0, 100, 100)
+        pygame.display.update(r1)
+
+        r2 = pygame.Rect(-10, 0, 100, 100)
+        pygame.display.update(r2)
+
+        r3 = pygame.Rect(-10, 0, -100, -100)
+        pygame.display.update(r3)
+
     def test_set_blocked__event_sequence(self):
-        """Ensure a sequence of event types can be blocked."""
         event_types = [
             pygame.KEYDOWN,
             pygame.KEYUP
@@ -30,3 +38,24 @@ class DisplayModuleTest(unittest.TestCase):
         ]
 
         pygame.event.set_blocked(event_types)
+
+    def test_flip(self):
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+        # Тест без изменений
+        self.assertIsNone(pygame.display.flip())
+
+        # Тест с изменениями
+        pygame.Surface.fill(screen, (PURPLE))
+        self.assertIsNone(pygame.display.flip())
+
+        # Тест без инициализации дисплея
+        pygame.display.quit()
+        with self.assertRaises(pygame.error):
+            (pygame.display.flip())
+
+        # Тест без окна
+        del screen
+        with self.assertRaises(pygame.error):
+            (pygame.display.flip())
+
